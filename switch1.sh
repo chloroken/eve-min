e21857c76603cee2c759ec88c482adb4aabc4b9d
chloroken <72576960+chloroken@users.noreply.github.com>
2025-08-09 03:54:17 +0000

    Update switch1.sh

--- switch1.sh
+++ switch1.sh
@@ -6,7 +6,7 @@ mapfile -t pids_array < "$pids_file"
 
 # Minimize clients
 tar="${pids_array["0"]}" # switch1.sh is 0, switch2.sh is 1, etc
-for pid in $(~/Documents/way-switcher/kdotool search --classname "steam_app_8500")
+for pid in $(~/Documents/eve-minimizer/kdotool search --classname "steam_app_8500")
 do
 	if [ "$pid" != "$tar" ]; then
 		~/Documents/eve-minimizer/kdotool windowminimize "$pid"

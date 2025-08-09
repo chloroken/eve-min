ffe4d91e7d9f64b6bb490f5c242aec41e468b82a
chloroken <72576960+chloroken@users.noreply.github.com>
2025-08-09 03:54:10 +0000

    Update switch1.sh

--- switch1.sh
+++ switch1.sh
@@ -9,9 +9,9 @@ tar="${pids_array["0"]}" # switch1.sh is 0, switch2.sh is 1, etc
 for pid in $(~/Documents/way-switcher/kdotool search --classname "steam_app_8500")
 do
 	if [ "$pid" != "$tar" ]; then
-		~/Documents/way-switcher/kdotool windowminimize "$pid"
+		~/Documents/eve-minimizer/kdotool windowminimize "$pid"
 	fi
 done
 
 # Tab to target client
-~/Documents/way-switcher/kdotool windowactivate "$tar"
+~/Documents/eve-minimizer/kdotool windowactivate "$tar"

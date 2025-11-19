fc29f0e805ee03e6d2c8ef106708161faf97d197
chloroken <72576960+chloroken@users.noreply.github.com>
2025-11-19 21:18:19 +0000

    use internalId instead of captionNormal

--- switch.js
+++ switch.js
@@ -1,7 +1,7 @@
 var windows = workspace.windowList();
 for (var i = 0; i < windows.length; ++i) {
   var w = windows[i];
-  if (w.captionNormal == "$TARGET") {
+  if (w.internalId == "$TARGET") {
     w.minimized = false;
     w.keepAbove = true;
   } else if (w.resourceClass == "steam_app_8500") {

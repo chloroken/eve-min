a5b15216793e04675712684e856e2750979c4576
chloroken <72576960+chloroken@users.noreply.github.com>
2025-12-12 10:19:14 +0000

    Prevent minimizing of launcher

--- switch.js
+++ switch.js
@@ -4,7 +4,8 @@ for (var i = 0; i < windows.length; ++i) {
   if (w.internalId == "$TARGET") {
     w.minimized = false;
     w.keepAbove = true;
-  } else if (w.resourceClass == "steam_app_8500") {
+  } else if ((w.resourceClass == "steam_app_8500") && (w.captionNormal != "EVE Launcher")) {
+    // Use "steam_app_default" for Lutris
     w.minimized = true;
     w.keepAbove = false;
   }

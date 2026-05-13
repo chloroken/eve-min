7250b2a29ad6ea9e5d14406afdeb3639d1c18d5f
chloroken <72576960+chloroken@users.noreply.github.com>
2026-05-13 17:23:34 +0000

    Update switch.js

--- switch.js
+++ switch.js
@@ -2,9 +2,11 @@ var windows = workspace.windowList();
 for (var i = 0; i < windows.length; ++i) {
   var w = windows[i];
   if (w.internalId == "$TARGET") {
+    w.keepAbove = true;
     workspace.activeWindow = w;
   } else if ((w.resourceClass == "steam_app_8500") && (w.captionNormal != "EVE Launcher")) {
     // Use "steam_app_default" for Lutris
     w.minimized = true;
+    w.keepAbove = false;
   }
 }
